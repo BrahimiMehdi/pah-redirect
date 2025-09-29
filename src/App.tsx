0abbc1e1d51d10c189b45e2b021510cc7e60c21a
BrahimiMehdi <brahimi.mehdi37@gmail.com>
2025-09-29 23:52:06 +0100

    redirect

---
+++ src/App.tsx
@@ -0,0 +1,32 @@
+import { useEffect } from 'react'
+import './App.css'
+
+function App() {
+  // Auto-redirect to the new domain
+  useEffect(() => {
+    window.location.href = 'https://pharma-at-hand.app';
+  }, []);
+
+  const handleRedirect = () => {
+    window.location.href = 'https://pharma-at-hand.app';
+  };
+
+  return (
+    <div className="app">
+      <div className="container">
+        <h1 className="title">Pharma at Hand</h1>
+        <p className="description">
+          We moved domains to <strong>pharma-at-hand.app</strong>
+        </p>
+        <button className="redirect-button" onClick={handleRedirect}>
+          Go to pharma-at-hand.app
+        </button>
+        <p className="redirect-text">
+          Redirecting you automatically...
+        </p>
+      </div>
+    </div>
+  )
+}
+
+export default App
